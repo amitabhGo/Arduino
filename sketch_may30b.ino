cc8e70068116fb361e3328d79bbb6c827d661b0f
Amitabh Gogoi <84743639+amitabhGo@users.noreply.github.com>
2021-05-30 16:46:49 +0530

    Add files via upload

---
+++ sketch_may30b.ino
@@ -0,0 +1,39 @@
+/* 
+ *  LDR SENSOR
+ *  
+ *  30-05-21
+ *  
+ */
+#define IN 8
+#define OUT 13
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(IN, INPUT);
+  pinMode(OUT, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int value = digitalRead(IN);
+
+  if(value == 1){
+
+    digitalWrite(OUT, HIGH);
+
+    Serial.println("LIGHT IS ON");
+    
+  }
+  else{
+
+    digitalWrite(OUT, LOW);
+    Serial.println("LIGHT IS OFF");
+    
+  }
+
+  delay(200);
+}
